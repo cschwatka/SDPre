f02f85bab440b7c5410b146cf59160de99c31009
Chase Schwatka <chaseschwatka@Chases-MacBook-Air.local>
2022-01-31 13:31:50 -0700

    Objective 1 and 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Chase Schwatka";
+    int age = 37;
+    String state = "Washington";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
